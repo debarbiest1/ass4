1a4b438991905a9a101f60f69afb98b1b4e51b2b
Kumissay <kkapammell@gmail.com>
2024-05-25 20:47:56 +0500

    commit2

--- src/main/java/org/example/Vertex.java
+++ src/main/java/org/example/Vertex.java
@@ -1,5 +1,11 @@
 package org.example;
 
 public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>,Double> adjVertices;
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjVertices.put(destination, weight);
+    }
 
 }
